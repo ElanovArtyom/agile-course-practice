dc42f4a6b630c70cd2706c2d5281d2203c9517e5
ElanovArtyom <elanoff.artyom@yandex.ru>
2017-02-26 04:36:13 +0300

    edit build.gradle

--- code/ElanovArtyom/View/src/test/java/ru/unn/agille/IntersectTwoLine/view/FakeViewTest.java
+++ code/ElanovArtyom/View/src/test/java/ru/unn/agille/IntersectTwoLine/view/FakeViewTest.java
@@ -5,7 +5,8 @@ import org.junit.Test;
 import static org.junit.Assert.fail;
 
 public class FakeViewTest {
-    @Ignore @Test
+    @Ignore
+    @Test
     public void fakeTest() {
         fail("No tests for view");
     }
