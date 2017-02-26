dbbc5e5d4161ea91d2e74d76ce1fa8d86f96b99e
ElanovArtyom <elanoff.artyom@yandex.ru>
2017-02-26 03:44:39 +0300

    create bind View and ViewModel

--- code/ElanovArtyom/View/src/main/java/ru/unn/agille/IntersectTwoLine/view/LineSegment2DView.java
+++ code/ElanovArtyom/View/src/main/java/ru/unn/agille/IntersectTwoLine/view/LineSegment2DView.java
@@ -1,24 +1,24 @@
 package ru.unn.agille.IntersectTwoLine.view;
 
+import ru.unn.agile.IntersectTwoLine.viewmodel.ViewModel;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 /**
  * Created by Artyom on 021 21.02.17.
  */
-public class LineSegment2DView {
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("LineSegment2DView");
-        frame.setContentPane(new LineSegment2DView().main_panel);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
-    }
+public final class LineSegment2DView {
 
-    private JPanel main_panel;
+    private ViewModel viewModel;
+    private JPanel mainPanel;
     private JTextField a1TextField;
     private JTextField b1TextField;
     private JTextField c1TextField;
-    private JButton Button;
+    private JButton buttonCheck;
     private JTextField textSolve;
     private JLabel lbSolve;
     private JTextField b2TextField;
@@ -26,7 +26,62 @@ public class LineSegment2DView {
     private JTextField c2TextField;
     private JLabel lbStatus;
 
-    private void createUIComponents() {
-        // TODO: place custom component creation code here
+    private LineSegment2DView() {
+
+    }
+
+    private LineSegment2DView(final ViewModel viewModel) {
+        this.viewModel = viewModel;
+        backBind();
+
+
+        buttonCheck.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent actionEvent) {
+                bind();
+                LineSegment2DView.this.viewModel.checkIntersect();
+                backBind();
+            }
+        });
+
+
+        KeyAdapter keyListener = new KeyAdapter() {
+            public void keyReleased(final KeyEvent e) {
+                bind();
+                LineSegment2DView.this.viewModel.processKeyInTextField(e.getKeyCode());
+                backBind();
+            }
+        };
+        a1TextField.addKeyListener(keyListener);
+        b1TextField.addKeyListener(keyListener);
+        c1TextField.addKeyListener(keyListener);
+        a2TextField.addKeyListener(keyListener);
+        b2TextField.addKeyListener(keyListener);
+        c2TextField.addKeyListener(keyListener);
+    }
+
+    public static void main(final String[] args) {
+        JFrame frame = new JFrame("LineSegment2DView");
+        frame.setContentPane(new LineSegment2DView(new ViewModel()).mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private void bind() {
+        viewModel.setA1(a1TextField.getText());
+        viewModel.setB1(b1TextField.getText());
+        viewModel.setC1(c1TextField.getText());
+        viewModel.setA2(a2TextField.getText());
+        viewModel.setB2(b2TextField.getText());
+        viewModel.setC2(c2TextField.getText());
+
+    }
+
+    private void backBind() {
+        buttonCheck.setEnabled(viewModel.isCalculateButtonEnabled());
+
+        textSolve.setText(viewModel.getResult());
+        lbStatus.setText(viewModel.getStatus());
     }
 }
